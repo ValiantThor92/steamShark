6f2f8e30a62e59b2e8d9cce42ddfbbb72fb263f5
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-07 11:28:16 -0700

    add steam api route in server.js

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+app.use(steam(SteamAPI_Init));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
