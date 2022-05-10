35c0b1bfbd9b2ece58b7318c587daf387eb4ed29
Re1d0n <reiduninnes@gmail.com>
2022-05-09 20:28:59 -0700

    added imageroutes.js

---
+++ controllers/api/image-Routes.js
@@ -0,0 +1,24 @@
+
+   
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const upload = require('../../utils/upload');
+const { Image } = require('../../models');
+
+// route to post an image, splits its filepath and saves the last part in the database along with the user_id
+router.post('/', withAuth, upload.single('image'), async (req, res) => {
+    const filepath = req.file.path.split("\\")
+    try {
+        const newImage = await Image.create({
+            path_name: filepath[2],
+            user_id: req.session.user_id,
+        });
+        res.render('upload', {logged_in: req.session.logged_in})
+    } catch (err) {
+        alert(err)
+        res.render('upload')
+    }
+
+});
+
+module.exports = router;
