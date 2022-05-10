e6147ed5f87b123b8e60eb39ed73882a2e51aa02
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-09 20:17:00 -0700

    add image api route to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const imageRoutes = require('./imageRoutes');
 
+router.use('/images', imageRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
