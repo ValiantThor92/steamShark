271a0223177c23518c155d050842a50afb27d087
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-10 19:28:34 -0700

    remove files and directories not needed for new direction

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
 const imageRoutes = require('./image-Routes');
 
 router.use('/images', imageRoutes);
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+
 
 module.exports = router;
