39318938d91bf50f062f67b569b4c6befb81cf48
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-10 20:22:47 -0700

    add user and index models

--- models/index.js
+++ models/index.js
@@ -1,2 +1,13 @@
 const User = require("./User");
 const Image = require("./Image");
+
+User.hasMany(Image, {
+  foreignKey: "user_id",
+  onDelete: 'CASCADE'
+})
+
+Image.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Image };
