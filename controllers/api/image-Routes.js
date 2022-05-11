01274796fc9e0e34b9bf9994f4700516f4f40886
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-05-11 09:41:50 -0700

    add boilerplate delete route for image

--- controllers/api/image-Routes.js
+++ controllers/api/image-Routes.js
@@ -21,4 +21,23 @@ router.post('/', withAuth, upload.single('image'), async (req, res) => {
 
 });
 
+// delete a photo
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+      const imageData = await Image.destroy({
+          where: {
+              id: req.params.id,
+              user_id: req.session.user_id,
+          },
+      });
+      if (!imageData) {
+          res.status(404).json({ message: 'Could not find a image with that id' })
+      }
+
+      res.status(200).json(imageData);
+  } catch (err) {
+      res.status(400).json(err);
+  }
+});
+
 module.exports = router;
