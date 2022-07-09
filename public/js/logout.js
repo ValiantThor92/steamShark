d973760ded06778363bafa7d62f8f766bec242a1
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-07-09 08:11:37 -0700

    alter logout for debug

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,14 @@
-async function logout() {
+const logout = async () => {
   const response = await fetch('/api/users/logout', {
-    method: 'post',
-    headers: { 'Content-Type': 'application/json' }
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
     document.location.replace('/login');
   } else {
-    alert(response.statusText);
+    alert("Failed to log out, please try again later");
   }
-}
+};
 
 document.querySelector('#logout').addEventListener('click', logout);
