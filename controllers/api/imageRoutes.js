6a1944792267da9af4aab63e150b48f560805164
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-07-09 08:29:43 -0700

    comment out delete func until i can fix

--- controllers/api/imageRoutes.js
+++ controllers/api/imageRoutes.js
@@ -21,24 +21,24 @@ router.post('/album', withAuth, upload.single('image'), (req, res) => {
 });
 
 // delete a photo
-router.delete('/:id', withAuth, async (req, res) => {
+// router.delete('/:id', withAuth, async (req, res) => {
 
-  try {
-      const imageData = await Image.destroy({
-          where: {
-              id: req.params.id,
-              user_id: req.session.user_id,
-          },
-      });
-      if (!imageData) {
-          res.status(404).json({ message: 'Could not find a image with that id' })
-      }
+//   try {
+//       const imageData = await Image.destroy({
+//           where: {
+//               id: req.params.id,
+//               user_id: req.session.user_id,
+//           },
+//       });
+//       if (!imageData) {
+//           res.status(404).json({ message: 'Could not find a image with that id' })
+//       }
 
-      res.status(200).json(imageData);
-  } catch (err) {
-      res.status(400).json(err);
-  }
+//       res.status(200).json(imageData);
+//   } catch (err) {
+//       res.status(400).json(err);
+//   }
 
-});
+// });
 
 module.exports = router;
--- public/js/delete.js
+++ public/js/delete.js
@@ -1,21 +1,21 @@
-async function deleteFormHandler(event) {
-  event.preventDefault();
-    try {
-      const imageData = await Image.destroy({
-          where: {
-              id: req.params.id,
-              user_id: req.session.user_id,
-          },
-      });
+// async function deleteFormHandler(event) {
+//   event.preventDefault();
+//     try {
+//       const imageData = await Image.destroy({
+//           where: {
+//               id: req.params.id,
+//               user_id: req.session.user_id,
+//           },
+//       });
 
-      if (!imageData) {
-        res.status(404).json({ message: 'Could not find a image with that id' })
-      }
+//       if (!imageData) {
+//         res.status(404).json({ message: 'Could not find a image with that id' })
+//       }
 
-      res.status(200).json(imageData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-};
+//       res.status(200).json(imageData);
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+// };
 
-document.querySelector('.delete').addEventListener('click', deleteFormHandler);
+// document.querySelector('.delete').addEventListener('click', deleteFormHandler);
